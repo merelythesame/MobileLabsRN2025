94e87919d472d9eb606e6e8f53ffd19dcf0be806
1TopG <ipz232_pov@student.ztu.edu.ua>
2025-05-18 13:13:50 +0300

    [FEATURE] Navigation covered with authprovider

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { AuthContext, AuthProvider } from 'context/AuthContext';
+import AppStack from 'navigation/AppStack';
+import GuestStack from 'navigation/GuestStack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <AuthProvider>
+        <Navigation />
+      </AuthProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Navigation = () => {
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) return null;
+
+  return (
+      <NavigationContainer>
+        {user ? <AppStack /> : <GuestStack />}
+      </NavigationContainer>
+  );
+};
