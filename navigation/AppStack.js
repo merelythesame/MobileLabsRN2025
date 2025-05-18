39c1134bfbad6e7c3a9787dfbdbec17ceba4bdfb
1TopG <ipz232_pov@student.ztu.edu.ua>
2025-05-18 13:15:06 +0300

    [FEATURE] Created navigation stack for logged user

---
+++ navigation/AppStack.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import ProfileScreen from '../screens/ProfileScreen';
+import EditProfileScreen from '../screens/EditProfileScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppStack() {
+    return (
+        <Stack.Navigator initialRouteName="Profile">
+            <Stack.Screen name="Profile" component={ProfileScreen} />
+            <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+        </Stack.Navigator>
+    );
+};
+
