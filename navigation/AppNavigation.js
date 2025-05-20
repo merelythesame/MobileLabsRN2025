874869e6ec3d3da2bcd1821d646eddefdf6fba6f
1TopG <ipz232_pov@student.ztu.edu.ua>
2025-05-20 13:28:39 +0300

    [FEATURE] Added Product list screen

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import ProductListScreen from '../screens/ProductListScreen';
+
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
---
+++ screens/ProductListScreen.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { View, Text, Button, FlatList, Image, StyleSheet } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../redux/slices/cartSlice';
+import { setProducts } from '../redux/slices/productsSlice';
+
+const productsData = [
+    { id: '1', name: 'Tomato', description: 'Fresh tomato just from land', price: 100, image: '../assets/tomato.png' },
+    { id: '2', name: 'Apple', description: 'Apple from the tree', price: 200, image: '../assets/apple.png' },
+];
+
+export default function ProductListScreen({ navigation }) {
+    const dispatch = useDispatch();
+    const products = useSelector(state => state.products);
+
+    useEffect(() => {
+        dispatch(setProducts(productsData));
+    }, []);
+
+    return (
+        <View>
+            <Button title="Go to Cart" onPress={() => navigation.navigate('Cart')} />
+            <FlatList
+                data={products}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <View style={styles.item}>
+                        <Image source={{ uri: item.image }} style={styles.image} />
+                        <Text>{item.name}</Text>
+                        <Text>{item.description}</Text>
+                        <Text>${item.price}</Text>
+                        <Button title="Add to Cart" onPress={() => dispatch(addToCart(item))} />
+                    </View>
+                )}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    item: { padding: 10, margin: 10, borderWidth: 1, borderColor: '#ccc' },
+    image: { width: 100, height: 100 },
+});
