33d5c3837cf6b0a1417eab675fcb0c64e8073760
1TopG <ipz232_pov@student.ztu.edu.ua>
2025-05-20 13:22:00 +0300

    [FEATURE] Added navigation

---
+++ navigation/AppNavigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Products">
+                <Stack.Screen name="Products" component={ProductListScreen} />
+                <Stack.Screen name="Cart" component={CartScreen} />
+                <Stack.Screen name="Checkout" component={CheckoutScreen} />
+                <Stack.Screen name="Orders" component={OrdersHistoryScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
